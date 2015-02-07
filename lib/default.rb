04c862a06f19049c4410c8ccd523646ccdbfec62
Mickaël Rémond <mremond@process-one.net>
2015-02-07 10:55:00 +0100

    More clean-up
    - Remove unneeded images.
    - More blogs reference removal.

--- lib/default.rb
+++ lib/default.rb
@@ -1,16 +1,3 @@
 # All files in the 'lib' directory will be loaded
 # before nanoc starts compiling.
-include Nanoc::Helpers::Rendering, Nanoc::Helpers::Blogging, BlogEntriesHelper
-
-module PostHelper
-  def get_post_start(post)
-    content = post.compiled_content
-    if content =~ /\s<!-- more -->\s/
-      content = content.partition('<!-- more -->').first +
-          "&bull;&nbsp;&bull;&nbsp;&bull; &nbsp;&nbsp; <div class='read-more'><a href='#{post.path}'>Continue reading &rsaquo;</a></div>"
-    end
-    return content
-  end
-end
-
-include PostHelper
+include Nanoc::Helpers::Rendering
