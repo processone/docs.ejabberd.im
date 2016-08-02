7e7fa90e76c47ae201723982e8fda29b7fd0fbd8
Marek Foss <mf@marekfoss.org>
2016-08-02 13:06:46 +0200

    Removed bodyclass from generator script

--- script/generate-guide-html.pl
+++ script/generate-guide-html.pl
@@ -43,7 +43,6 @@ my $in;
 
 $in="---
 title: ejabberd Installation and Operation Guide
-bodyclass: nocomment
 HTML header: <style>$css</style>
     <link rel=\"stylesheet\" href=\"https://cdnjs.cloudflare.com/ajax/libs/highlight.js/8.8.0/styles/solarized_dark.min.css\">
     <script src=\"https://cdnjs.cloudflare.com/ajax/libs/highlight.js/8.8.0/highlight.min.js\"></script>
